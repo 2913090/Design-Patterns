52ea1f12dee2f5c52b362534f0126b17970b47b4
Magdesyan_Kirill <107571018+2913090@users.noreply.github.com>
2024-06-20 10:26:35 +0300

    Add files via upload

---
+++ patternStrategy.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class ShippingCostStrategy {
+public:
+    virtual ~ShippingCostStrategy() = default;
+
+    virtual double calculateCost(double weight) = 0;
+};
+
+class GroundShipping : public ShippingCostStrategy {
+public:
+    double calculateCost(double weight) {
+        return weight * 1.5;
+    }
+};
+
+class AirShipping : public ShippingCostStrategy {
+public:
+    double calculateCost(double weight) {
+        return weight * 3;
+    }
+};
+
+class WaterShipping : public ShippingCostStrategy {
+public:
+    double calculateCost(double weight) {
+        return weight * 2.2;
+    }
+};
+
+class Order {
+private:
+    double weight;
+    ShippingCostStrategy* strategy;
+public:
+    Order(double weight, ShippingCostStrategy* strategy) :weight(weight), strategy(strategy){}
+
+    void setShippingStrategy(ShippingCostStrategy* strategy) {
+        this->strategy = strategy;
+    }
+
+    double calculateShippingCost() {
+        return strategy->calculateCost(weight);
+    }
+};
+
+
+
+
+int main()
+{
+    GroundShipping ground;
+    AirShipping air;
+    WaterShipping water;
+
+    Order order(10.0, &ground);
+    cout << order.calculateShippingCost() << endl;
+    order.setShippingStrategy(&air);
+    cout << order.calculateShippingCost() << endl;
+    order.setShippingStrategy(&water);
+    cout << order.calculateShippingCost() << endl;
+}
